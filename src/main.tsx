26a9d78c9cb911296eb15da3c5042ddba240dafe
lukakhutsishvili <lukakhutsishvili777@gmail.com>
2025-02-23 19:09:10 +0400

    ads

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { init } from "@telegram-apps/sdk-react";
 
-init();
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
